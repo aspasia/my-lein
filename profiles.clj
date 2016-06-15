a998425c300481afc4820687f526fc6d15011776
aspasia <aspasia.beneti@gmail.com>
2016-06-15 10:44:43 +0200

    adding the profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,10 @@
+{:user {
+ :plugins [[cider/cider-nrepl "0.12.0"]
+	   #_[org.clojure/clojure "1.8.0"]
+	   #_[cider/cider-nrepl "0.13.0-SNAPSHOT"]
+           [refactor-nrepl "1.1.0"]
+           [lein-kibit "0.1.2"]
+           [lein-bikeshed "0.3.0"]
+           [jonase/eastwood "0.2.3"]
+	   [s3-wagon-private "1.2.0"]
+           #_[im.chit/vinyasa "0.4.5"]]}}
