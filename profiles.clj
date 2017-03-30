0569adb568b9fb6369aaa68472519f586bd29476
Aspasia Beneti <aspasia.beneti@gmail.com>
2017-03-30 13:39:37 +0200

    current profiles lein

---
+++ profiles.clj
@@ -0,0 +1,12 @@
+{:user {
+ :plugins [[cider/cider-nrepl "0.12.0"]
+	   [org.clojure/clojure "1.8.0"]
+	   [cider/cider-nrepl "0.15.0-SNAPSHOT"]
+           [refactor-nrepl "1.1.0"]
+           [lein-kibit "0.1.2"]
+           [lein-bikeshed "0.3.0"]
+           [jonase/eastwood "0.2.3"]
+	   [s3-wagon-private "1.2.0" :exclusions [commons-codec]]
+           [lein-ancient "0.6.10"]
+           #_[im.chit/vinyasa "0.4.5"]
+	   [venantius/yagni "0.1.4"]]}}
