a6f1bf72ac01db0d62f77c4bb744720e2adbc998
Aspasia Beneti <aspasia.beneti@gmail.com>
2017-04-25 18:11:25 +0200

    Current status of profiles with test refresh

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,13 @@
 {:user {
- :plugins [[cider/cider-nrepl "0.12.0"]
+ :plugins [;[cider/cider-nrepl "0.12.0"]
 	   [org.clojure/clojure "1.8.0"]
 	   [cider/cider-nrepl "0.15.0-SNAPSHOT"]
-           [refactor-nrepl "1.1.0"]
+           ;[refactor-nrepl "1.1.0"]
            [lein-kibit "0.1.2"]
            [lein-bikeshed "0.3.0"]
            [jonase/eastwood "0.2.3"]
+           [com.jakemccrary/lein-test-refresh "0.19.0"]
 	   [s3-wagon-private "1.2.0" :exclusions [commons-codec]]
-           [lein-ancient "0.6.10"]
+           [lein-ancient "0.6.10" :exclusions [org.apache.httpcomponents/httpcore org.apache.httpcomponents/httpclient]]
            #_[im.chit/vinyasa "0.4.5"]
-	   [venantius/yagni "0.1.4"]]}}
+	   #_[venantius/yagni "0.1.4" :exclusions [org.clojure/tools.namespace]]]}}
